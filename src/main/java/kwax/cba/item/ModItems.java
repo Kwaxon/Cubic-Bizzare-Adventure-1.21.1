4e317a182097470c63c3f3f4219ae368614ceb5a
Kwaxon <illiavilbitskiy@gmail.com>
2026-05-11 22:23:49 +0100

    structers folder

--- src/main/java/kwax/cba/item/ModItems.java
+++ src/main/java/kwax/cba/item/ModItems.java
@@ -14,7 +14,7 @@ public class ModItems {
     public static final Item Arrow_Shard_1 = registerItem("arrow_shard_1", new Item(new Item.Settings().maxCount(4)));
     public static final Item Arrow_Shard_2 = registerItem("arrow_shard_2", new Item(new Item.Settings().maxCount(4)));
 
-
+//
     private static Item registerItem(String name, Item item) {
         return Registry.register(Registries.ITEM, Identifier.of(CubicBizzareAdventure.MOD_ID, name), item);
     }
