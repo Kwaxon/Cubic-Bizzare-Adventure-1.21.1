642fa4fa1bfb954dc8659ec6d09132c085c47b14
Kwaxon <illiavilbitskiy@gmail.com>
2026-05-11 17:26:48 +0100

    idk

---
+++ src/client/java/kwax/cba/client/CubicBizzareAdventureClient.java
@@ -0,0 +1,10 @@
+package kwax.cba.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CubicBizzareAdventureClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
