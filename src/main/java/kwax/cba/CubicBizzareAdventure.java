d434f049388c880c7dd7ca6927b649a76889c9b4
Kwaxon <illiavilbitskiy@gmail.com>
2026-05-13 19:33:23 +0100

    drops and nbt

--- src/main/java/kwax/cba/CubicBizzareAdventure.java
+++ src/main/java/kwax/cba/CubicBizzareAdventure.java
@@ -3,13 +3,6 @@ package kwax.cba;
 import java.util.HashSet;
 import java.util.Set;
 import java.util.UUID;
-
-import kwax.cba.bloodlines.Bloodlines;
-import kwax.cba.component.ModDataComponentTypes;
-import kwax.cba.misc.ModCommands;
-import kwax.cba.misc.ModStates;
-import net.fabricmc.fabric.api.networking.v1.ServerPlayConnectionEvents;
-import net.minecraft.text.Text;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -29,10 +22,6 @@ public class CubicBizzareAdventure implements ModInitializer {
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 	public static final Set<UUID> FROZEN_PLAYERS = new HashSet<>();
 
-	private boolean isFirstJoin(ServerPlayerEntity player) {
-		return true;
-	};
-
 	@Override
 	public void onInitialize() {
 		// This code runs as soon as Minecraft is in a mod-load-ready state.
@@ -42,34 +31,6 @@ public class CubicBizzareAdventure implements ModInitializer {
 		ModItems.registerModItems();
 		ModBlocks.registerModBlocks();
 		ModItemGroups.registerItemGroup();
-		ModDataComponentTypes.registerComponents();
-		ModCommands.registerCommands();
-
-		LOGGER.info("test 0.0.0.1v");
-		LOGGER.info("Hello Fabric world!");
-
-		ServerPlayConnectionEvents.JOIN.register((handler, sender, server) -> {
-			ServerPlayerEntity player = handler.getPlayer();
-			ModStates state = ModStates.getServerState(server);
-
-			CubicBizzareAdventure.LOGGER.info(player.getUuid().toString());
-
-			String[] table = Bloodlines.getBloodlines();
-
-			if (!state.joinedPlayers.contains(player.getUuid())) {
-				state.joinedPlayers.add(player.getUuid());
-
-				String family = table[player.getRandom().nextInt(table.length)];
-				state.bloodlines.put(player.getUuid(), family);
-
-				state.markDirty();
-
-				player.sendMessage(Text.literal("You were born as "+ family));
-			}
-
-
-		});
-
 
 		ServerTickEvents.END_SERVER_TICK.register(server -> {
 			for (ServerPlayerEntity player : server.getPlayerManager().getPlayerList()) {
